03cf1bb324347a75cd35e4b54011d3ddd7b62af5
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2022-10-13 17:54:10 +0530

    Update settings.py

--- pusherfeed/settings.py
+++ pusherfeed/settings.py
@@ -76,9 +76,7 @@ WSGI_APPLICATION = 'pusherfeed.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-MAX_CONN_AGE = 600
 
 DATABASES = {
     "default": {
@@ -86,15 +84,6 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
-
-if "DATABASE_URL" in os.environ:
-    # Configure Django for DATABASE_URL environment variable.
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=MAX_CONN_AGE, ssl_require=True)
-
-    # Enable test database if found in CI environment.
-    if "CI" in os.environ:
-        DATABASES["default"]["TEST"] = DATABASES["default"]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
